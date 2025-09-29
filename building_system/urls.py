f316772ea725e86caf064d3259b3f8454c6d86cc
adrian <ro.ysla@duocuc.cl>
2025-09-29 14:36:50 -0300

    adding URL for page2

--- building_system/urls.py
+++ building_system/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
     path('/escanear/', views.escanear, name='escanear'),
+    path('/page2/', views.page2, name='page2'),
     path('building/<int:building_id>/', views.building_detail, name='building_detail')
 ]
